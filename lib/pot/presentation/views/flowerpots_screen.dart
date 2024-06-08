248e87216a4783af9208329b8664779c34ce2e39
Sebastian Ramirez <89022108+SebastianRamirez-16@users.noreply.github.com>
2024-06-07 20:48:23 -0500

    feat(add): add widget card_plant

--- lib/pot/presentation/views/flowerpots_screen.dart
+++ lib/pot/presentation/views/flowerpots_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:ztech_mobile_application/common/widgets/diagonal_background_painter.dart';
 import 'package:ztech_mobile_application/common/widgets/navigation_appbar.dart';
 import 'package:ztech_mobile_application/pot/presentation/widgets/top_bar.dart';
+import 'package:ztech_mobile_application/pot/domain/user.dart'; // Asegúrate de que la ruta de importación esté correcta
+import 'package:ztech_mobile_application/pot/presentation/widgets/card_plant.dart'; // Importa tu widget CardPlant
+import 'package:ztech_mobile_application/pot/infrastructure/user_repository.dart'; // Importa el UserRepository
 
 class FlowerpotsScreen extends StatefulWidget {
   @override
@@ -10,6 +13,15 @@ class FlowerpotsScreen extends StatefulWidget {
 
 class _FlowerpotsScreenState extends State<FlowerpotsScreen> {
   int _selectedIndex = 0; // Este índice representa la pantalla "Flowerpots"
+  Future<User>? user;
+
+  @override
+  void initState() {
+    super.initState();
+    UserRepository userRepository =
+        UserRepository(); // Instancia de tu UserRepository
+    user = userRepository.getUser(); // Obtiene el usuario y las plantas
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +29,33 @@ class _FlowerpotsScreenState extends State<FlowerpotsScreen> {
       appBar: TopBar(), // Usa tu AppBar personalizado
       body: CustomPaint(
         painter: DiagonalBackgroundPainter(),
-        child: Center(
-          child: Text("Bienvenido a la pantalla de Flowerpots"),
+        child: FutureBuilder<User>(
+          future: user,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                return GridView.builder(
+                  padding: EdgeInsets.all(8),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Dos columnas
+                    crossAxisSpacing: 10, // Espacio horizontal
+                    mainAxisSpacing: 10, // Espacio vertical
+                    childAspectRatio: 0.8, // Aspecto de las tarjetas
+                  ),
+                  itemCount: snapshot.data!.plants.length,
+                  itemBuilder: (context, index) {
+                    return CardPlant(plantInfo: snapshot.data!.plants[index]);
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                    child: Text('Error al cargar datos: ${snapshot.error}'));
+              }
+            }
+            return Center(
+                child:
+                    CircularProgressIndicator()); // Muestra un spinner de carga
+          },
         ),
       ),
       bottomNavigationBar: CustomNavigationBar(
