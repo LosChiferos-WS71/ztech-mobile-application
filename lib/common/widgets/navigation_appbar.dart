74bf11c7fe6c8bdaa852e385224ba91b683d276c
Aldo Francisco Pastrana Leon <U20211C186@upc.edu.pe>
2024-06-08 17:16:57 -0500

    Update Navigation App Bar

--- lib/common/widgets/navigation_appbar.dart
+++ lib/common/widgets/navigation_appbar.dart
@@ -1,62 +1,74 @@
 import 'package:flutter/material.dart';
 
-class CustomNavigationBar extends StatelessWidget {
+class CustomNavigationBar extends StatefulWidget {
   final int selectedIndex;
   final ValueChanged<int> onDestinationSelected;
 
   const CustomNavigationBar({
-    super.key,
+    Key? key,
     required this.selectedIndex,
     required this.onDestinationSelected,
-  });
+  }) : super(key: key);
+
+  @override
+  _CustomNavigationBarState createState() => _CustomNavigationBarState();
+}
+
+class _CustomNavigationBarState extends State<CustomNavigationBar> {
+  late int selectedIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedIndex = widget.selectedIndex;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final routeName = ModalRoute.of(context)?.settings.name;
+    final index = _getIndexFromRouteName(routeName);
+    if (index != null && index != selectedIndex) {
+      setState(() {
+        selectedIndex = index;
+      });
+    }
+  }
+
+  int? _getIndexFromRouteName(String? routeName) {
+    final routes = {
+      'flowerpots': 0,
+      'addPot': 1,
+      'home': 2,
+      'profile': 3,
+    };
+    return routes[routeName];
+  }
 
   @override
   Widget build(BuildContext context) {
     return NavigationBar(
       selectedIndex: selectedIndex,
       onDestinationSelected: (index) {
-        // Notificar al widget padre
-        onDestinationSelected(index);
-        // Navegar a la nueva pantalla
+        setState(() {
+          selectedIndex = index;
+        });
+        widget.onDestinationSelected(index);
         _navigateToScreen(index, context);
       },
       destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.local_florist),
-          label: 'Flowerpots',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.add),
-          label: 'Add',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.nature),
-          label: 'Plants',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
+        NavigationDestination(icon: Icon(Icons.local_florist), label: 'Flowerpots'),
+        NavigationDestination(icon: Icon(Icons.add), label: 'Add'),
+        NavigationDestination(icon: Icon(Icons.nature), label: 'Plants'),
+        NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
       ],
     );
   }
 
   void _navigateToScreen(int index, BuildContext context) {
-    switch (index) {
-      case 0:
-        Navigator.pushNamed(context, 'flowerpots');
-        break;
-      case 1:
-        Navigator.pushNamed(context, 'addPot');
-        break;
-      case 2:
-        Navigator.pushNamed(context, 'home');
-        break;
-      case 3:
-        Navigator.pushNamed(context, 'profile');
-        break;
-      default:
-        Navigator.pushNamed(context, '/');
+    final routes = ['flowerpots', 'addPot', 'home', 'profile'];
+    if (ModalRoute.of(context)?.settings.name != routes[index]) {
+      Navigator.pushNamed(context, routes[index]);
     }
   }
 }
