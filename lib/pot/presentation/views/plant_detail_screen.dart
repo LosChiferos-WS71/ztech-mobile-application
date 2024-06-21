c52b14933416b95be2484b22cfddbc7ceeba7119
Sebastian Ramirez <89022108+SebastianRamirez-16@users.noreply.github.com>
2024-06-20 20:49:52 -0500

    feat(add): Add view of plant section

--- lib/pot/presentation/views/plant_detail_screen.dart
+++ lib/pot/presentation/views/plant_detail_screen.dart
@@ -14,24 +14,41 @@ class PlantDetailScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Image.network(
               plant.plantImageUrl,
               fit: BoxFit.cover,
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                plant.plantName,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Container(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      plant.plantName,
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 13.0),
+                    child: Text(
+                      "${plant.scientificName}",
+                      style:
+                          TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(0.0),
+              padding: const EdgeInsets.only(top: 5.0, left: 14),
               child: Text(
-                "Scientific Name: ${plant.scientificName}",
-                style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+                "Description:",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
             Padding(
@@ -41,12 +58,11 @@ class PlantDetailScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 16),
               ),
             ),
-            Divider(),
             Padding(
-              padding: const EdgeInsets.all(5.0),
+              padding: const EdgeInsets.only(top: 14.0, left: 14),
               child: Text(
                 "Recommendations:",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
             Padding(
@@ -56,12 +72,11 @@ class PlantDetailScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 16),
               ),
             ),
-            Divider(),
             Padding(
-              padding: const EdgeInsets.all(5.0),
+              padding: const EdgeInsets.only(top: 14.0, left: 14),
               child: Text(
                 "Care Instructions:",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
             ListTile(
@@ -83,6 +98,32 @@ class PlantDetailScreen extends StatelessWidget {
               title: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text("Water: ${plant.careIcons['water']}"),
+              ),
+            ),
+            //insert a buttom
+            Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Add plant to user's collection
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      backgroundColor: Color(0xFF276749),
+                    ),
+                    child: Text(
+                      "Add me to a pot",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
